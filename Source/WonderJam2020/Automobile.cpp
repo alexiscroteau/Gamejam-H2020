cde192d6a8172eb38a27f0dbbe219e74e85cd51c
Alexandra <amen.alexandra@gmail.com>
2020-03-01 12:51:27 -0500

    Replication / vie

--- Source/WonderJam2020/Automobile.cpp
+++ Source/WonderJam2020/Automobile.cpp
@@ -3,3 +3,29 @@
 
 #include "Automobile.h"
 
+void AAutomobile::Touche_Implementation(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+	if (HasAuthority()) {
+
+			vie -= 1;
+			if (vie < 0) {
+				AAutomobile* voiture = (AAutomobile*)OtherActor;
+				if (voiture->IsA(AAutomobile::StaticClass())) {
+					voiture->AjouterPoint();
+			}
+		}
+	}
+}
+
+bool AAutomobile::Touche_Validate(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+	return true;
+}
+
+void AAutomobile::AjouterPoint_Implementation() {
+	if (HasAuthority()) {
+		point += 1;
+	}
+}
+
+bool AAutomobile::AjouterPoint_Validate() {
+	return true;
+}
--- Source/WonderJam2020/Automobile.h
+++ Source/WonderJam2020/Automobile.h
@@ -15,6 +15,16 @@ class WONDERJAM2020_API AAutomobile : public AWheeledVehicle
 	GENERATED_BODY()
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=VieVoiture)
-	int32 vie = 100;
+	int vie = 100;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = pOINTVoiture)
+	int point = 0;
 	
+	UFUNCTION(Server, Reliable, WithValidation)
+		virtual void Touche(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION(Server, Reliable, WithValidation)
+		void AjouterPoint();
+
+
+
 };
